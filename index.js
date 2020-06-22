c62cd7d9399e6b191c018fbacd970fa1e6f79679
Jamie Fu <jamie.fu@gmail.com>
2020-06-22 16:48:21 -0400

    im giving up after this

--- index.js
+++ index.js
@@ -94,17 +94,17 @@ app.get('/webhook', (req, res) => {
 function handleMessage(sender_psid, received_message) {
 
   let response = {
-    "text": 'this is wonky now isnt it'
+    "text": "this is wonky now isnt it"
   }
 
   // Check if the message contains text
-  if (received_message.text) {    
+  // if (received_message.text) {    
 
-    // Create the payload for a basic text message
-    response = {
-      "text": 'You sent the message: "${received_message.text}". Now send me an image!'
-    }
-  }
+  //   // Create the payload for a basic text message
+  //   response = {
+  //     "text": 'You sent the message: "${received_message.text}". Now send me an image!'
+  //   }
+  // }
 
   // Sends the response message
   callSendAPI(sender_psid, response); 
@@ -119,6 +119,7 @@ function callSendAPI(sender_psid, response) {
     "message": response
   }
 
+  console.log(request_body)
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
@@ -137,9 +138,4 @@ function callSendAPI(sender_psid, response) {
 // Handles messaging_postbacks events
 // function handlePostback(sender_psid, received_postback) {
 
-// }
-
-// Sends response messages via the Send API
-// function callSendAPI(sender_psid, response) {
-  
 // }
